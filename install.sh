116612d96971c57b6e16f6668f4a545b55a85753
Zeanon <thezeanon@gmail.com>
2023-10-18 08:38:55 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+CONFIG_PATH="${HOME}/printer_data/config"
+KLICKY_PATH="${HOME}/klicky"
+
+set -eu
+export LC_ALL=C
+
+
+function preflight_checks {
+    if [ "$EUID" -eq 0 ]; then
+        echo "[PRE-CHECK] This script must not be run as root!"
+        exit -1
+    fi
+
+    if [ "$(sudo systemctl list-units --full -all -t service --no-legend | grep -F 'klipper.service')" ]; then
+        printf "[PRE-CHECK] Klipper service found! Continuing...\n\n"
+    else
+        echo "[ERROR] Klipper service not found, please install Klipper first!"
+        exit -1
+    fi
+}
+
+function check_download {
+    local klickydirname klickybasename
+    klickydirname="$(dirname ${KLICKY_PATH})"
+    klickybasename="$(basename ${KLICKY_PATH})"
+
+    if [ ! -d "${KLICKY_PATH}" ]; then
+        echo "[DOWNLOAD] Downloading Klicky repository..."
+        if git -C $klickydirname clone https://github.com/LynxCrew/Klicky.git $klickybasename; then
+            chmod +x ${KLICKY_PATH}/install.sh
+            printf "[DOWNLOAD] Download complete!\n\n"
+        else
+            echo "[ERROR] Download of Autotune TMC git repository failed!"
+            exit -1
+        fi
+    else
+        printf "[DOWNLOAD] Klicky repository already found locally. Continuing...\n\n"
+    fi
+}
+
+function link_extension {
+    echo "[INSTALL] Linking extension to Klipper..."
+    ln -srfn "${KLICKY_PATH}/_include.cfg" "${CONFIG_PATH}/Klicky/_include.cfg"
+    ln -srfn "${KLICKY_PATH}/macros.cfg" "${CONFIG_PATH}/Klicky/macros.cfg"
+    ln -srfn "${KLICKY_PATH}/database.cfg" "${CONFIG_PATH}/Klicky/database.cfg"
+	FILE=${CONFIG_PATH}/Variables/klicky_variables.cfg
+	if [ -f "$FILE" ]; then
+		echo "Variables file already exists"
+	else
+		cp "${KLICKY_PATH}/klicky_variables.cfg" "${CONFIG_PATH}/Variables/klicky_variables.cfg"
+	fi
+}
+
+function restart_klipper {
+    echo "[POST-INSTALL] Restarting Klipper..."
+    sudo systemctl restart klipper
+}
+
+
+printf "\n======================================\n"
+echo "- Klicky install script -"
+printf "======================================\n\n"
+
+
+# Run steps
+preflight_checks
+check_download
+link_extension
+restart_klipper
