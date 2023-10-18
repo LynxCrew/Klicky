41c5b387d989a331387065913625689d1c6cda99
Zeanon <thezeanon@gmail.com>
2023-10-18 19:52:58 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,13 +3,18 @@
 KLICKY_PATH="${HOME}/klicky"
 
 function update_repo {
-    echo "[UPDATE] Updating Klicky repository..."
-    cd ${KLICKY_PATH}
-    if git pull origin; then
-        printf "[UPDATE] Download complete!\n\n"
+    if git rev-list HEAD...origin/main --count > 0; then
+        echo "[UPDATE] Updating Klicky repository..."
+        cd ${KLICKY_PATH}
+        if git pull origin; then
+            printf "[UPDATE] Download complete!\n\n"
+        else
+            echo "[ERROR] Download of Klicky git repository failed!"
+            exit -1
+        fi
     else
-        echo "[ERROR] Download of Klicky git repository failed!"
-        exit -1
+        echo "[UPDATE] Repo already up to date."
+        exit 0
     fi
 }
 
